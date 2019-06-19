b5d7dbeb80aa9fa09cad5ea016918a3c6301a540
IanTorrente <ianpaolotorrente@gmail.com>
2019-06-19 01:04:11 -0700

    added desktop styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './Styles/Mobile/mobile-style.css';
+import './Styles/style.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
