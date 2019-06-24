87cb9610399e1d60bc34f3cf5e360358a1ed8749
IanTorrente <ianpaolotorrente@gmail.com>
2019-06-23 21:28:23 -0700

    change routing from local to heroku app

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  API_ENDPOINT: "https://afternoon-ocean-86123.herokuapp.com",
+  API_ENDPOINT: "https://afternoon-ocean-86123.herokuapp.com/",
   API_KEY: process.env.REACT_APP_API_KEY,
   API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/"
 }
